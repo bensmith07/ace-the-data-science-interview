7c62620bc863fbef04b59d60ea3f17ee7e71a295
Ben Smith <ben.f.smith07@gmail.com>
2022-06-27 11:05:50 -0500

    ...

---
+++ temp_table_for_8-7.sql
@@ -0,0 +1,78 @@
+/* DROP TABLE events;
+DROP TABLE events2; */
+
+CREATE TEMPORARY TABLE purchases (
+  purchase_id INT UNSIGNED,
+  user_id INT UNSIGNED,
+  product_id INT UNSIGNED,
+  quantity INT,
+  price FLOAT,
+  purchase_time DATETIME
+)
+;
+  
+  
+INSERT INTO events
+  VALUES (1, 'impression', '2019-01-01'),
+     (1, 'impression', '2019-01-01'),
+         (1, 'impression', '2019-01-01'),
+         (1, 'impression', '2019-01-01'),
+         (1, 'impression', '2019-01-01'),
+         (1, 'impression', '2019-01-01'),
+         (1, 'impression', '2019-01-01'),
+         (1, 'impression', '2019-01-01'),
+         (1, 'impression', '2019-01-01'),
+         (1, 'impression', '2019-01-01'),
+         (1, 'click', '2019-01-01'),
+         (1, 'click', '2019-01-01'),
+         (1, 'click', '2019-01-01'),
+         (1, 'click', '2019-01-01'),
+         (1, 'click', '2019-01-01'),      
+         (2, 'impression', '2019-01-01'),
+         (2, 'impression', '2019-01-01'),
+         (2, 'impression', '2019-01-01'),
+         (2, 'impression', '2019-01-01'),
+         (2, 'impression', '2019-01-01'),
+         (2, 'impression', '2019-01-01'),
+         (2, 'impression', '2019-01-01'),
+         (2, 'impression', '2019-01-01'),
+         (2, 'impression', '2019-01-01'),
+         (2, 'impression', '2019-01-01'),
+         (2, 'click', '2019-01-01'),
+         (2, 'click', '2019-01-01'),
+         (1, 'impression', '2019-02-02')
+;
+   
+CREATE TEMPORARY TABLE events2 SELECT * FROM events
+;
+
+/* SELECT app_id, 
+       (
+        COUNT(event_id) / (
+                            SELECT COUNT(event_id)
+                                FROM events2
+                                					 WHERE event_id = 'impression'                                					 				AND event_date BETWEEN '2019-01-01' AND '2019-01-01'
+                                GROUP BY app_id                                
+                                
+                                    
+                            )
+        )
+  FROM events 
+   WHERE event_id = 'click'
+      AND event_date BETWEEN '2019-01-01' AND '2019-01-01'
+  GROUP BY app_id  
+  
+     
+; */
+
+SELECT 
+       app_id, 
+       SUM(IF(event_id = 'click', 1, 0)) / SUM(IF(event_id = 'impression', 1, 0))
+       FROM events
+       WHERE event_date = '2019-01-01'
+       GROUP BY app_id
+       ;
+       
+
+
+  
