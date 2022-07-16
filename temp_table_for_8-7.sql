ca4a5d4cb734c8c6b16ca9298280514f39d57e7e
Ben Smith <ben.f.smith07@gmail.com>
2022-07-16 08:10:39 -0500

    ...

--- temp_table_for_8-7.sql
+++ temp_table_for_8-7.sql
@@ -1,5 +1,4 @@
-/* DROP TABLE events;
-DROP TABLE events2; */
+/* DROP TABLE purchases; */
 
 CREATE TEMPORARY TABLE purchases (
   purchase_id INT UNSIGNED,
@@ -7,72 +6,47 @@ CREATE TEMPORARY TABLE purchases (
   product_id INT UNSIGNED,
   quantity INT,
   price FLOAT,
-  purchase_time DATETIME
+  purchase_time DATE
 )
 ;
   
-  
-INSERT INTO events
-  VALUES (1, 'impression', '2019-01-01'),
-     (1, 'impression', '2019-01-01'),
-         (1, 'impression', '2019-01-01'),
-         (1, 'impression', '2019-01-01'),
-         (1, 'impression', '2019-01-01'),
-         (1, 'impression', '2019-01-01'),
-         (1, 'impression', '2019-01-01'),
-         (1, 'impression', '2019-01-01'),
-         (1, 'impression', '2019-01-01'),
-         (1, 'impression', '2019-01-01'),
-         (1, 'click', '2019-01-01'),
-         (1, 'click', '2019-01-01'),
-         (1, 'click', '2019-01-01'),
-         (1, 'click', '2019-01-01'),
-         (1, 'click', '2019-01-01'),      
-         (2, 'impression', '2019-01-01'),
-         (2, 'impression', '2019-01-01'),
-         (2, 'impression', '2019-01-01'),
-         (2, 'impression', '2019-01-01'),
-         (2, 'impression', '2019-01-01'),
-         (2, 'impression', '2019-01-01'),
-         (2, 'impression', '2019-01-01'),
-         (2, 'impression', '2019-01-01'),
-         (2, 'impression', '2019-01-01'),
-         (2, 'impression', '2019-01-01'),
-         (2, 'click', '2019-01-01'),
-         (2, 'click', '2019-01-01'),
-         (1, 'impression', '2019-02-02')
+INSERT INTO events (purchase_id, user_id, product_id, quantity, price, purchase_time)
+            VALUES (0001,        1111,    100,        1,        4.99,  '2021-01-01'),
+                   (0002,        1111,    100,        1,        4.99,  '2021-01-02'),
+                   (0003,        1112,    200,        1,        6.50,  '2021-01-03'),
+                   (0004,        1113,    200,        1,       10.00,  '2021-01-04')
 ;
    
-CREATE TEMPORARY TABLE events2 SELECT * FROM events
-;
+-- SELECT COUNT(DISTINCT user_id)
+--   FROM (
+--         SELECT user_id, 
+--                RANK() OVER (
+--                             PARTITION BY user_id,
+--                                          product_id,
+--                             ORDER BY CAST(purchase_time AS DATE)
+--                            ) AS purchase_no
+--           FROM purchases
+--         ) t 
+--   WHERE purchase_no = 2
+-- ;
 
-/* SELECT app_id, 
-       (
-        COUNT(event_id) / (
-                            SELECT COUNT(event_id)
-                                FROM events2
-                                					 WHERE event_id = 'impression'                                					 				AND event_date BETWEEN '2019-01-01' AND '2019-01-01'
-                                GROUP BY app_id                                
-                                
-                                    
-                            )
-        )
-  FROM events 
-   WHERE event_id = 'click'
-      AND event_date BETWEEN '2019-01-01' AND '2019-01-01'
-  GROUP BY app_id  
-  
-     
-; */
-
-SELECT 
-       app_id, 
-       SUM(IF(event_id = 'click', 1, 0)) / SUM(IF(event_id = 'impression', 1, 0))
-       FROM events
-       WHERE event_date = '2019-01-01'
-       GROUP BY app_id
-       ;
-       
+SELECT user_id, 
+       COUNT(DISTINCT user_id) 
+  FROM purchases
+  WHERE user_id IN (
+                    SELECT user_iD
+                      FROM ( 
+                            SELECT user_id, 
+                                product_id,
+                                date(datefield), 
+                                COUNT(*) as n_purchases
+                            FROM purchases
+                            GROUP BY user_id, 
+                                    product_id,
+                                    date(datefield)
+                            HAVING n_purchases > 1
+                            ) t
+                    )
+  GROUP BY user_id
 
 
-  
